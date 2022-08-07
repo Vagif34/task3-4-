510c2c38d47bf8566bd4000eb0e728a815a8243c
Vagif34 <vagifrg@code.edu.az>
2022-08-07 23:23:51 +0400

    new file

---
+++ task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = { 3, 5, 7, 9, 12, 23 };
+            int num =int.Parse(Console.ReadLine());
+           
+            Console.WriteLine(Method(arr,num));
+        }
+        static bool Method(int []arr,int num)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+
+                    return true;
+            }
+            return false;
+            
+            
+            
+                
+            
+        }
+    }
+}
